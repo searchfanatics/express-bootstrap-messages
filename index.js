ec7f9172f611be42d25282053bfff7af26838457
David Murdoch <musicisair@yahoo.com>
2012-06-13 15:50:44 -0300

    Add bootstrap changes

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
-
 /*!
  * Express - Contrib - messages
- * Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
+ * Original Copyright(c) 2010 TJ Holowaychuk <tj@vision-media.ca>
  * MIT Licensed
+ * Bootstrap adaptation Copyright(c) 2012 David Murdoch <bootstrap-messages@davidmurdoch.com>
  */
 
+var http = require('http');
+
+var oldFlash = http.IncomingMessage.prototype.flash;
+
+http.IncomingMessage.prototype.flash = function(type, msg){
+  var old = oldFlash.apply(this, arguments);
+
+  if (type && msg) {
+    if( msg.text ){
+      var old = oldFlash.call(this)[type]
+        , oldmsg = old.pop()
+        , header = msg.header
+        , text = msg.text;
+        old.push(msg);
+      }
+      return old;
+    }
+    return old;
+  };
+
 module.exports = function(req, res){
+
   return function(){
     var buf = []
       , messages = req.flash()
       , types = Object.keys(messages)
       , len = types.length;
     if (!len) return '';
-    buf.push('<div id="messages">');
+    buf.push('<div class="alerts">');
     for (var i = 0; i < len; ++i) {
       var type = types[i]
         , msgs = messages[type];
       if (msgs) {
-        buf.push('  <ul class="' + type + '">');
         for (var j = 0, l = msgs.length; j < l; ++j) {
-          var msg = msgs[j];
-          buf.push('    <li>' + msg + '</li>');
+          var msg = msgs[j]
+            , heading = msg.heading
+            , text = heading ? (msg.text || "") : msg;
+          buf.push('  <div class="alert alert-' + type + (heading ? ' alert-block' : '') + '">');
+          if(heading){
+            buf.push('    <h4 class="alert-heading">' + heading + '</h4>');
+          }
+          buf.push('    <button type="button" class="close" data-dismiss="alert">×</button>');
+          buf.push('    ' + text);
+          buf.push('  </div>');
         }
-        buf.push('  </ul>');
       }
     }
     buf.push('</div>');
